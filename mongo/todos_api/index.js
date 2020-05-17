fb9b4ccd0f14170c884173aa19a3937453f4e7a1
seatacken <57050116+seatacken@users.noreply.github.com>
2020-05-17 13:00:06 -0700

    pre mongo install

--- mongo/todos_api/index.js
+++ mongo/todos_api/index.js
@@ -7,12 +7,14 @@ require('dotenv').config()
 //app.listen() to start server
 //app.get() defining routes
 app.get('/', function(req, res){ //when requesting from ex. 3000/
-  res.send('hi from express')
+  res.send('hi from express') //res.send is dynamic depending on content passed in
+  //or res.send({message: 'hi from obj'}) //sent as json
+  //or res.json({message: 'hi from obj'}) //same, more explicit, stringifys things, recog as json by postman
 })
 app.get('/happy', function(req, res){ //http://localhost:3000/happy
   res.send(':)')
 })
 
-app.listen(port, function(){ //points to env file where PORT is declared. in c9, .env is already set
+app.listen(process.env.PORT, function(){ //points to env file where PORT is declared. in c9, .env is already set
   console.log('app is running on port ' + port);
 }) //need to privide a port, for cloud9, simply use process.env.PORT rather than 3000 or whatever
