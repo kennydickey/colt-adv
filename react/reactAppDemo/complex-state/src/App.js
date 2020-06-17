df9bff6602a539511c3cb6a7adc5cf80ac99d7dd
seatacken <57050116+seatacken@users.noreply.github.com>
2020-06-16 23:30:05 -0700

    complex state notes

--- react/reactAppDemo/complex-state/src/App.js
+++ react/reactAppDemo/complex-state/src/App.js
@@ -22,7 +22,49 @@ class App extends Component {
         }
       ]
     };
-  }
+
+    //to randomly change content
+    setTimeout(() => { //arrow fn to preserve 'this' in our callback functions, so that this .setState refers to the right 'this', App in this case
+      // this.setState
+      //find random instructor and output num between 1 and 4
+      const randInst = Math.floor(Math.random() * this.state.instructors.length)
+      //choose random hobby from within arr of hobbies
+      const hobbyIndex = Math.floor(Math.random() * this.state.instructors[randInst].length); //accesses instrctors[2].length, also outputs a num between 1 and 2
+
+      //still within setTimeout
+      // const instructors = this.state.instructors.slice(); //slice gives new arr
+      // // make a copy of obj we want to mutate
+      // //try new name for arr here
+      // instructors[randInst] = Object.assign({}, instructors[randInst]);
+      // //modify only hobbies arr, use slice to make another new arr
+      // instructors[randInst].hobbies = instructors[randInst].hobbies.slice();
+      // //now we can use splice to modify new arr
+      // instructors[randInst].hobbies.splice(hobbyIndex, 1);
+      // this.setState({instructors}); //instructors obj is made, no need for key/val
+
+      const instructors = this.state.instructors.map((inst, i) => { //returns new arr
+        if(i === randInst) { //if index we are mapping === inst we want to modify
+          const hobbies = inst.hobbies.slice(); //copy of hobbies arr
+          //or const hobbies = [...inst.hobbies]; same using spread ^^
+          hobbies.splice(hobbyIndex, 1); //use on new arr to remove random from index 1
+          return {
+            ...inst, //return new obj with the spread of what inst has left
+            hobbies // plus hobbies arr we just copied and modified
+          };
+        }
+        return inst; //otherwise return original inst
+        //alternatively..
+        // const hobbies = inst.hobbies.slice(); //copy of hobbies arr
+        // i === randInst ? { //if i === randInst
+        //   ...inst, // to keep all of the keys in our obj
+        //   hobbies: [...inst.hobbies.slice(0, hobbyIndex).concat(inst.hobbies.slice(hobbyIndex+1, inst.hobbies.length))]
+        // } : inst; // if not, then return original instructor
+      });
+      this.setState({instructors});
+    }, 5000)
+
+  } // ^within props
+
   render() {
     const instructors = this.state.instructors.map((instructor, index) => (
       <li key={index}>
